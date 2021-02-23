7b73ef4ec2346f037b739b6376943230af211ccd
Kaiser-G <kaiserg6969@gmail.com>
2021-02-23 18:11:28 +0100

    Première version

---
+++ script.js
@@ -0,0 +1,16 @@
+const toggleButton = document.getElementById("toggle-button");
+const sideBar = document.getElementById("side-bar");
+
+toggleButton.addEventListener("click", show);
+
+function show() {
+  sideBar.classList.toggle("active");
+}
+
+// remove sidebar if click on the main content
+
+const content = document.querySelector(".content");
+
+content.addEventListener("click", () => {
+  sideBar.classList.remove("active");
+});
